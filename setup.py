b581b4f3916f9f0edcda94d7811dd35c45483f1c
austinfatt <afatt90@gmail.com>
2021-04-12 14:45:32 -0400

    Modifies: setup.py removes the strict versioning of the required packages. This causes a userwarning in           bandstructure.py when using x_ticks instead of FixedLocator

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 __version__ = '1.0.0'
 
 REQUIRES = [
-    'coverage==4.3.4',
-    'cycler==0.10.0',
-    'kiwisolver==1.3.1',
-    'matplotlib==3.1.0',
-    'numpy==1.20.1',
-    'pyparsing==2.4.7',
-    'python-dateutil==2.8.1',
-    'six==1.15.0'
+    'coverage',
+    'cycler',
+    'kiwisolver',
+    'matplotlib',
+    'numpy',
+    'pyparsing',
+    'python-dateutil',
+    'six'
 ]
 
 setup(
